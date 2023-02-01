7d1f9e8b816ceb0f27aa044a1dfde137d6c0b27d
Nke0628 <ooo_diva@yahoo.co.jp>
2023-02-01 20:41:28 +0900

    NB-10 全体高さの修正、言語を日本語に

--- blog/app/src/components/layout/Container.tsx
+++ blog/app/src/components/layout/Container.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Container: React.FC<Props> = ({ children }) => {
   return (
-    <div className="bg-gray-900 h-screen text-gray-200">
+    <div className="bg-gray-900 h-full text-gray-200">
       <div className="container mx-auto px-4 py-2 md:px-20">{children}</div>
     </div>
   )
