1d183a36fe73b1dba944de2de7e46b7220281cc9
Nke0628 <ooo_diva@yahoo.co.jp>
2023-02-14 22:15:54 +0900

    NB-15 profileページ作成

--- blog/app/src/pages/profile/index.tsx
+++ blog/app/src/pages/profile/index.tsx
@@ -19,22 +19,27 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
       <main className="py-8 md:w-3/5 mx-auto">
         <table className="w-full">
           <tbody className="">
-            <tr className="border-t-[0.5px]">
+            <tr className="border-t-[1px]">
+              <td className="font-bold">Account</td>
+              <td className="p-8">
+                <a href="" className="text-sky-500">
+                  <p>準備中</p>
+                  <p>GitHub、Zenn</p>
+                </a>
+              </td>
+            </tr>
+            <tr className="border-t-[1px]">
               <td className="font-bold">Job</td>
               <td className="p-8">Software Enginner</td>
             </tr>
-            <tr className="border-t-[0.5px]">
-              <td className="font-bold">🖥　Job</td>
-              <td className="p-8">Software Enginner</td>
-            </tr>
-            <tr className="border-t-[0.5px]">
-              <td className="font-bold">👨　About</td>
+            <tr className="border-t-[1px]">
+              <td className="font-bold">About</td>
               <td className="p-8">
-                PHP、Typescript。
-                <br />
-                バックエンドがメインですが、Next.js(React)も勉強中。
-                <br />
-                コードを書くことが好き。
+                <p>PHP、Laravel、Typescript。</p>
+                <p>
+                  バックエンドがメインですが、このブログもNext.js(React)で書いてます。
+                </p>
+                <p>コードや設計が好き。</p>
               </td>
             </tr>
           </tbody>
