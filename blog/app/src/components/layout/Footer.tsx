3885d9419c68aacea140a790a920aa9a11cbd89d
Nke0628 <ooo_diva@yahoo.co.jp>
2023-02-14 22:40:25 +0900

    NB-17 fotter作成

---
+++ blog/app/src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer: React.FC<Props> = ({}) => {
+  return (
+    <div className="h-1/6 w-full mt-10 mb-10">
+      <div className="text-center">© Nake All rights reserved.</div>
+    </div>
+  )
+}
+
+export default Footer
--- blog/app/src/pages/_app.tsx
+++ blog/app/src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app'
 import Container from '@/components/layout/Container'
 import Header from '@/components/layout/Header'
 import '@/styles/index.css'
+import Footer from '@/components/layout/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header></Header>
       <Component {...pageProps} />
+      <Footer></Footer>
     </Container>
   )
 }
