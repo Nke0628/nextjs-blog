d1ee162a6c46f483e4206f237fcec5f95489644d
Nke0628 <ooo_diva@yahoo.co.jp>
2025-02-16 14:10:20 +0900

    #28 不要なコードを削除

--- blog/app/test/step_definitions/blog_top.ts
+++ blog/app/test/step_definitions/blog_top.ts
@@ -32,17 +32,6 @@ Given('トップページを開く', async () => {
   await page.goto('http://localhost:3000/')
 })
 
-// When(
-//   '検索ワード{string}を入力する',
-//   { timeout: 2 * 5000 },
-//   async (keyword: string) => {
-//     await page.getByLabel('検索', { exact: true }).click()
-//     await page.getByLabel('検索', { exact: true }).fill(keyword)
-//     await page.waitForTimeout(1000)
-//     await page.getByLabel('検索', { exact: true }).press('Enter')
-//   },
-// )
-
 Then('記事一覧が表示されていること', async () => {
   const articleList = await page.getByTestId('article')
   console.log(articleList)
